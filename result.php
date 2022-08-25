834f50e2a5c2b87da322a84626ba7eb44188e40b
Dhruvsolanki345 <dhruvsolanki753@gmail.com>
2022-08-25 15:51:00 +0530

    Add home url to result page

--- result.php
+++ result.php
@@ -38,7 +38,7 @@
 
 <body class="bg-light">
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3">
-    <a class="navbar-brand" href="#">VotingHub</a>
+    <a class="navbar-brand" href="home.php">VotingHub</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
       aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
